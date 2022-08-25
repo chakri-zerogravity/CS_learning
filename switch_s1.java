e87032613d54b527b3a4cd83e99c32389755e8fd
chakri <chakradhar969@gmail.com>
2022-08-25 08:32:03 +0530

    Switch statements

---
+++ switch_s1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class switch_s1 {
+    
+    public static void main(String[] args) {
+
+        try (Scanner s1 = new Scanner(System.in)) {
+            int a = s1.nextInt();
+
+            switch (a){
+                case 1:
+                System.out.println("This is case 2");
+
+                break;
+      
+
+            case 2:
+            System.out.println("This is case 2");
+
+            break;
+
+            }
+        }
+        
+    }
+}
