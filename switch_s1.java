dd12914b5c97c13138c9fca8b17ca78f6b4770e2
chakri <chakradhar969@gmail.com>
2022-08-25 08:43:47 +0530

    Added print statement

--- switch_s1.java
+++ switch_s1.java
@@ -4,9 +4,11 @@ public class switch_s1 {
     public static void main(String[] args) {
 
         try (Scanner s1 = new Scanner(System.in)) {
+            System.out.println("Enter a number 1 or 2");
             int a = s1.nextInt();
 
             switch (a){
+
                 case 1:
                 System.out.println("This is case 2");
 
